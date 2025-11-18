6bf3eb77c670514e2636c607549e6cb56583afc1
huza <huzaifahasif3@gmail.com>
2025-11-18 10:02:12 +0700

    pulse anims

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
           We craft websites that are not only visually stunning but also strengthen your fashion brand's identity and make it easier for customers to find you online.
         </p>
       </div>
-      <div className="z-10 pb-16 animate-bounce">
+      <div className="z-10 pb-16 animate-pulse">
         <p className="text-sm mb-2">Scroll Down</p>
         <svg className="mx-auto w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
